349da48a47fa6f82e892dab847fbddf5e5b6580e
karina-d-larson <karina.d.larson@gmail.com>
2026-01-09 22:37:47 -0700

    prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeLetter;
+
+        Console.Write("What is your Grade %? ");
+        string grade = Console.ReadLine();
+
+        float gradeFloat = float.Parse(grade);
+
+        if (gradeFloat >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeFloat >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeFloat >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeFloat >= 60){
+            gradeLetter = "D";
+        }
+        else{
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {gradeLetter}");
+
+        if (gradeFloat >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Let's not procrastinate next time");
+        }
+
+        
     }
 }
