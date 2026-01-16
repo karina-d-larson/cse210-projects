e8736bb2c607ecd89eac42ea6d6118a4a847add5
karina-d-larson <karina.d.larson@gmail.com>
2026-01-15 21:03:57 -0700

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int magicNumberInt = int.Parse(magicNumber);
+        Random randomGenerator = new Random();
+        int magicNumberInt = randomGenerator.Next(1, 101);
+        bool run = true;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+
+            
+            if (guessNumber < magicNumberInt) {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumberInt) {
+                Console.WriteLine("Lower");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+                run = false;
+            }
+        }while (run == true);
+
     }
 }
