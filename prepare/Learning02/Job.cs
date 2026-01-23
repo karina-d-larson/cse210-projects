35fccb8cca350fe28d078ed3658e380229600edc
karina-d-larson <karina.d.larson@gmail.com>
2026-01-22 18:21:18 -0700

    finished prepare 3

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -5,4 +5,12 @@ public class Job
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
+
+    // behavior
+    public void Display()
+    {
+        string result;
+        result = $"{_jobTitle} ({_company}) {_startYear}-{_endYear}";
+        Console.WriteLine(result);
+    }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -2,5 +2,17 @@ public class Resume
 {
     // attributes
     public string _name;
-    public List<Job> _jobs;
+    public List<Job> _jobs = new List<Job>();
+    
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            // calling Display method on each job
+            job.Display();
+        }
+    }
 }
