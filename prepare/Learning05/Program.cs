357784a72ca2fb3eb28d520543c3bb7cac2e4a92
karina-d-larson <karina.d.larson@gmail.com>
2026-03-05 19:34:46 -0700

    prepare 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 5);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Green", 3, 7);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Purple", 3);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
