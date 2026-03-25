296f4d232879641a95205f01f8751d56993ce308
karina-d-larson <karina.d.larson@gmail.com>
2026-03-25 13:39:14 -0600

    section 1 of final

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create some comments
+        Comment comment1 = new Comment("Maggie", "Great video!");
+        Comment comment2 = new Comment("John", "I learned a lot.");
+        Comment comment3 = new Comment("Don", "Thanks for sharing!");
+
+        // Create a video
+        Video video = new Video("I'm bored", "Mickey", 300);
+
+        // Add comments to the video
+        video.AddComment(comment1);
+        video.AddComment(comment2);
+        video.AddComment(comment3);
+
+        // Display video details and comments
+        video.Display();
     }
 }
