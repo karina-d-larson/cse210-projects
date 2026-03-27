2206958522f12aa1d5046ba70751375b49b14664
karina-d-larson <karina.d.larson@gmail.com>
2026-03-26 20:52:08 -0600

    fixing 2

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -25,5 +25,28 @@ class Program
         Console.WriteLine(order.GetShippingLabel());
         Console.WriteLine();
         Console.WriteLine($"Total Cost: ${order.GetTotalCost():0.00}");
+        
+
+        // Create some products PART 2
+        Product product3 = new Product("Fake Laptop", "NOTP12", 120.00, 1);
+        Product product4 = new Product("Moose", "MSP456", 25.00, 2);
+
+        // Create an address
+        Address address2 = new Address("123 Maple St", "Springfield", "ID", "USA");
+
+        // Create a customer
+        Customer customer2 = new Customer("Karen", address2);
+
+        // Create an order
+        Order order2 = new Order(customer2);
+        order.AddProduct(product3);
+        order.AddProduct(product4);
+
+        // Display order info
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine();
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine();
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost():0.00}");
     }
 }
